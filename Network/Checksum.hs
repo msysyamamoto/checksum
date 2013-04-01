2c65152f15d007ce6d74c09743ff9ccdebc61a2c
ymmtmsys <m2yamamoto@gmail.com>
2013-04-02 07:52:19 +0900

    twice addHi16ToLow16

--- Network/Checksum.hs
+++ Network/Checksum.hs
@@ -4,7 +4,7 @@ import Data.Bits (complement, shiftL, shiftR, (.|.), (.&.))
 import Data.Word (Word8, Word16, Word32)
 
 inChecksum :: [Word8] -> Word16
-inChecksum ws = fromIntegral . complement . addCarry . addHi16ToLow16 $ ws'
+inChecksum ws = fromIntegral . complement . twice addHi16ToLow16 $ ws'
   where
     ws' = sum . map fromIntegral . packWord16 $ ws
 
@@ -22,5 +22,5 @@ word8ToWord16 high low = high' .|. low'
 addHi16ToLow16 :: Word32 -> Word32
 addHi16ToLow16 n = (n `shiftR` 16) + (n .&. 0xffff)
 
-addCarry :: Word32 -> Word32
-addCarry n = (n `shiftR` 16) + n
+twice :: (a -> a) -> a -> a
+twice f a  = f (f a)
